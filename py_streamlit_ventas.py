5d41d7236bc62f9a56f1972c61a74d8eb27381a5
adrianzeballossoliz-beep <adrianzeballossoliz@gmail.com>
2025-12-05 20:24:38 -0400

    Update py_streamlit_ventas.py

--- py_streamlit_ventas.py
+++ py_streamlit_ventas.py
@@ -15,7 +15,7 @@ st.set_page_config(
 )
 
 # CADENA DE CONEXIÓN CORRECTA PARA TU MYSQL LOCAL
-DEFAULT_DB_URI = "mysql+pymysql://root:localhost:3307/proyecto"
+DEFAULT_DB_URI = "mysql+pymysql://root:@localhost:3307/proyecto"
 
 # ============================================================
 # FUNCIÓN DE CONEXIÓN
